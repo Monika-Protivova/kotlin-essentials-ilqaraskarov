24a026f1c6fa2a0687246f14447fb9386eb73906
ilqaraskarov <ilqar.askerovv@gmail.com>
2025-06-10 20:50:05 +0400

    Lesson 1 completed

--- src/main/kotlin/com/motycka/edu/lesson01/Main.kt
+++ src/main/kotlin/com/motycka/edu/lesson01/Main.kt
@@ -9,7 +9,5 @@ package com.motycka.edu.lesson01
 
 fun main(args: Array<String>) {
     println("Hello, Kotlin!")
-    println("Program arguments: ${args.joinToString(" ")}")
-
-
+    println("Program arguments: ${args.joinToString(", ")}")
 }
